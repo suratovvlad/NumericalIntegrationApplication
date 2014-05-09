c3d9f9ec68cc0db1f3e4fdd763507163300bb4a6
vsuratov <suratovvlad@gmail.com>
2014-05-10 02:57:03 +0400

    Added trapezoidal rule component test

---
+++ NumericalIntegrationApplication/TrapezoidalRuleComponent/TrapezoidalRule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace TrapezoidalRuleComponent
+{
+    public class TrapezoidalRule : Component
+    {
+
+        public decimal CalculatePartitionCount(decimal a, decimal b, decimal error, decimal maxSecondDerivative)
+        {
+            decimal n = 0;
+
+            n = (b - a) * (b - a) * (b - a);
+            n /= (12 * error);
+            n *= maxSecondDerivative;
+            n = Convert.ToDecimal(Math.Sqrt(Convert.ToDouble(n)));
+
+            return n;
+        }
+
+        public decimal Calculate(decimal a, decimal b, decimal n, List<decimal> FunctionValues)
+        {
+            decimal result = 0;
+
+            for (int i = 1; i < FunctionValues.Count - 1; ++i)
+            {
+                result += FunctionValues[i];
+            }
+
+            result += ((FunctionValues[0] + FunctionValues[FunctionValues.Count - 1]) / 2);
+
+            result *= ((b - a) / n);
+
+            return result;
+        }
+    }
+
+
+}
